92d34505f69059df77fcb4421f5bbd2f8b2f3316
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-03-07 21:57:10 +0100

    fisheye lens script to control efects on camera methods to write in both scripts

---
+++ filmowcy_projectUnity/Assets/Scripts/CamEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamEffect : MonoBehaviour {
+
+	private EffectsControlling camera;
+	public float fishEyeValue;
+
+	void Start(){
+		camera = GameObject.Find ("Main Camera").GetComponent<EffectsControlling> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.gameObject.tag == "Player") {
+			camera.ChangeFisheye (fishEyeValue);
+			Destroy (gameObject);
+		}
+
+	}
+}
---
+++ filmowcy_projectUnity/Assets/Scripts/EffectsControlling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class EffectsControlling : MonoBehaviour {
+
+	private VignetteAndChromaticAberration vignette;
+	private NoiseAndGrain noise;
+	private Fisheye fisheye;
+	private Tonemapping tonemapping;
+	private MotionBlur motionBlur;
+
+	void Start () {
+		vignette = gameObject.GetComponent<VignetteAndChromaticAberration> ();
+		noise = gameObject.GetComponent<NoiseAndGrain> ();
+		fisheye = gameObject.GetComponent<Fisheye> ();
+		tonemapping = gameObject.GetComponent<Tonemapping> ();
+		motionBlur = gameObject.GetComponent<MotionBlur> ();
+	}
+	
+
+	public void ChangeFisheye(float value){
+		fisheye.strengthX = value;
+	}
+}
