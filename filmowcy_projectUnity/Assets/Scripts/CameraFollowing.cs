7d138c920c6b2128798ede9a5b0abf17987996dc
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-03-07 14:53:35 +0100

    imported image effects from standard assets copied scripts for moving and camera following some dev version of graphics

---
+++ filmowcy_projectUnity/Assets/Scripts/CameraFollowing.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollowing : MonoBehaviour {
+
+	public GameObject objectToFollow;
+	public float xBorder;
+	public float yBorder;
+
+	private Vector3 offset;
+	private bool changingBoth;
+
+	void Start () {
+		changingBoth = false;
+	}
+
+
+	void Update () {
+		offset = objectToFollow.transform.position - transform.position;
+
+		if (offset.x > xBorder && offset.y > yBorder) {
+			Vector3 offsetWhenChange = new Vector3 (xBorder, yBorder, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+			changingBoth = true;
+		}
+
+		if (offset.x > xBorder && offset.y < -yBorder) {
+			Vector3 offsetWhenChange = new Vector3 (xBorder, -yBorder, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+			changingBoth = true;
+		}
+
+		if (offset.x < -xBorder && offset.y > yBorder) {
+			Vector3 offsetWhenChange = new Vector3 (-xBorder, yBorder, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+			changingBoth = true;
+		}
+
+		if (offset.x < -xBorder && offset.y < -yBorder) {
+			Vector3 offsetWhenChange = new Vector3 (-xBorder, -yBorder, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+			changingBoth = true;
+		}
+
+
+
+
+		if (offset.x > xBorder && !changingBoth) {
+			Vector3 offsetWhenChange = new Vector3 (xBorder, offset.y, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+		}
+
+		if (offset.x < -xBorder && !changingBoth) {
+			Vector3 offsetWhenChange = new Vector3 (-xBorder, offset.y, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+		}
+
+		if (offset.y > yBorder && !changingBoth) {
+			Vector3 offsetWhenChange = new Vector3 (offset.x, yBorder, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+		}
+
+		if (offset.y < -yBorder && !changingBoth) {
+			Vector3 offsetWhenChange = new Vector3 (offset.x, -yBorder, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+		}
+
+		changingBoth = false;
+	}
+
+}
---
+++ filmowcy_projectUnity/Assets/Scripts/KeyboardMovement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class KeyboardMovement : MonoBehaviour {
+
+	[Header("Prędkość postaci")]
+	public float speed = 10f;
+	[Header("Siła skoku")]
+	public float jumpForce = 50f;
+	[Header("Maksymalna prędkość")]
+	public float maxSpeed = 7f;
+
+	private bool canJump;
+	private bool canSpeedUp;
+	private Rigidbody2D rb2d;
+	private float horizontal;
+	private float realSpeed;
+
+
+
+	public float Horizontal { 
+		get{
+			return horizontal;
+		}
+	}
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		realSpeed = speed;
+		canJump = true;
+		canSpeedUp = true;
+
+	}
+
+
+	void Update () {
+
+		horizontal = Input.GetAxis ("Horizontal");
+
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed) {
+			canSpeedUp = false;
+
+		}	else 
+			canSpeedUp = true;
+
+		if (rb2d.velocity.x * horizontal < 0) {
+			realSpeed += Mathf.Abs (rb2d.velocity.x);
+		} else
+			realSpeed = speed;
+
+		if ((Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow)) && canJump) {
+			rb2d.AddForce (Vector2.up * jumpForce * 3);
+
+			canJump = false;
+		}
+
+		if (Mathf.Abs(rb2d.velocity.y) > 0.1f && rb2d.gravityScale <= 3f) {
+			
+			rb2d.gravityScale += 0.07f;
+		}
+
+
+		if (Input.GetAxis ("Horizontal") == 0f) {
+			Break ();
+		}
+
+		if(canSpeedUp)
+			rb2d.AddForce (Vector2.right * horizontal * realSpeed);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "ground") {
+			canJump = true;
+			rb2d.gravityScale = 1;
+		}
+
+	}
+
+	void Break(){
+		
+		if (rb2d.velocity.x > 0f)
+			horizontal = -0.5f;
+		else if (rb2d.velocity.x < 0f)
+			horizontal = 0.5f;
+	
+	}
+}
