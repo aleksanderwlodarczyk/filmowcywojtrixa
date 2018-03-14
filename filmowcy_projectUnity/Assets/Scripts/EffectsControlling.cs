726bc831e696b7f4cd25d789047376c455bbd1de
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-03-14 23:41:42 +0100

    build ready, steering on screen to check

--- filmowcy_projectUnity/Assets/Scripts/EffectsControlling.cs
+++ filmowcy_projectUnity/Assets/Scripts/EffectsControlling.cs
@@ -17,24 +17,43 @@ public class EffectsControlling : MonoBehaviour {
 		fisheye = gameObject.GetComponent<Fisheye> ();
 		tonemapping = gameObject.GetComponent<Tonemapping> ();
 		motionBlur = gameObject.GetComponent<MotionBlur> ();
+
+		DisableAllEffects ();
 	}
-	
+
+	void DisableAllEffects(){
+		vignette.enabled = false;
+		noise.enabled = false;
+		fisheye.enabled = false;
+		tonemapping.enabled = false;
+		motionBlur.enabled = false;
+	}
 
 	public void ChangeCameraEffect(int effect, float value){
 		switch (effect) {
 		case 1:
+			DisableAllEffects ();
+			vignette.enabled = true;
 			vignette.intensity = value;
 			break;
 		case 2:
+			DisableAllEffects ();
+			noise.enabled = true;
 			noise.generalIntensity = value;
 			break;
 		case 3:
+			DisableAllEffects ();
+			fisheye.enabled = true;
 			fisheye.strengthX = value;
 			break;
 		case 4:
+			DisableAllEffects ();
+			tonemapping.enabled = true;
 			tonemapping.middleGrey = value;
 			break;
 		case 5:
+			DisableAllEffects ();
+			motionBlur.enabled = true;
 			motionBlur.blurAmount = value;
 			break;
 		}
