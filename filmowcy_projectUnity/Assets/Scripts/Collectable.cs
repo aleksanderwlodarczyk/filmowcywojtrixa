4001bf446be7dc1477bcddc3ae11bc6eb502bfe9
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-03-15 23:49:54 +0100

    dollar pickup score scripts no animation/no ui

---
+++ filmowcy_projectUnity/Assets/Scripts/Collectable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+	public int scoreToAdd;
+
+	private Score scoreHandler;
+
+	void Start(){
+		scoreHandler = GameObject.Find ("scoreHandler").GetComponent<Score> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			scoreHandler.AddScore (scoreToAdd);
+			Destroy (gameObject);
+		}
+	}
+
+}
---
+++ filmowcy_projectUnity/Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+	private int score;
+
+	public int ScoreAmount{
+		get { return score;}
+	}
+
+	void Start(){
+		score = 0;
+	}
+
+	public void AddScore(int amount){
+		score += amount;
+		Debug.Log (score);
+	}
+}
