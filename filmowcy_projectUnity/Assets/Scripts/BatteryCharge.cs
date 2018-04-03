4738ab5e3e0a86c64423149b14df1e394f1ef3c5
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-04-03 12:16:23 +0200

    battery charging pickup(still no effect when battery drops to 0)

---
+++ filmowcy_projectUnity/Assets/Scripts/BatteryCharge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryCharge : MonoBehaviour {
+
+    public float mAh;
+
+    private Battery batteryHandler;
+	void Start () {
+        batteryHandler = GameObject.Find("BatteryController").GetComponent<Battery>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            batteryHandler.AddPower(mAh);
+        }
+    }
+}
