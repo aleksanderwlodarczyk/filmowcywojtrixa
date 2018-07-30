1b80c5f1aa670aa085d5dddd79605766828d9b8d
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-07-31 01:35:37 +0200

    level select scene works

---
+++ filmowcy_projectUnity/Assets/Scripts/ButtonHighlighter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHighlighter : MonoBehaviour {
+
+    public Color highlightColor;
+
+    private MenuButton activeButton;
+
+    public void SetButtonActive(MenuButton button)
+    {
+        // reset previous button color
+        if(activeButton != null)
+        {
+            activeButton.buttonBackground.color = activeButton.startButtonColor;
+        }
+
+        // set new button color
+        button.buttonBackground.color = highlightColor;
+        activeButton = button;
+    }
+
+}
---
+++ filmowcy_projectUnity/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour {
+
+    private Transform leftPanelTransform;
+    private Button playButton;
+    private Text leftLevelTitle;
+    private Text leftLevelScore;
+    private Image leftLevelScreen;
+
+    private Text buttonLevelTitle;
+    private Text buttonLevelScore;
+
+    private SceneManaging scenes;
+
+    private ButtonHighlighter highlighter;
+    
+    public Image buttonBackground;
+    public Color startButtonColor;
+
+    public string levelTitle;
+    public int levelScore;
+    public int levelID;
+    
+    [SerializeField]
+    private Sprite levelScreen;
+
+    private void Start()
+    {
+        // find highlighter
+        highlighter = GameObject.FindObjectOfType<ButtonHighlighter>().GetComponent<ButtonHighlighter>();
+
+        // remember what color of level button is at start
+        buttonBackground = gameObject.transform.Find("frame").gameObject.GetComponent<Image>();
+        startButtonColor = buttonBackground.color;
+
+        scenes = GameObject.Find("scenes").GetComponent<SceneManaging>();
+
+        // find button textes
+        buttonLevelScore = gameObject.transform.Find("Text").gameObject.GetComponent<Text>();
+        buttonLevelTitle = gameObject.transform.Find("level_title").gameObject.GetComponent<Text>();
+
+        // set button textes
+        buttonLevelTitle.text = levelTitle;
+
+        // find left frame objects
+        leftPanelTransform = GameObject.Find("left_frame").transform;
+        leftLevelTitle = leftPanelTransform.Find("title").gameObject.GetComponent<Text>();
+        leftLevelScore = leftPanelTransform.Find("score").gameObject.GetComponent<Text>();
+        leftLevelScreen = leftPanelTransform.Find("level_screen").gameObject.GetComponent<Image>();
+        playButton = leftPanelTransform.Find("playButton").gameObject.GetComponent<Button>();
+
+    }
+
+    public void ButtonClicked()
+    {
+        // highlight the button
+        highlighter.SetButtonActive(this);
+
+        // remove listeners from previous buttons
+        playButton.onClick.RemoveAllListeners();
+
+        // set left frame objects
+        leftLevelScore.text = levelScore.ToString();
+        leftLevelTitle.text = levelTitle;
+        leftLevelScreen.overrideSprite = levelScreen;
+
+        // set play button
+        playButton.interactable = true;
+        playButton.onClick.AddListener(LoadMyLevel);
+    }
+
+    public void LoadMyLevel()
+    {
+        scenes.LoadLevel(levelID);
+    }
+}
