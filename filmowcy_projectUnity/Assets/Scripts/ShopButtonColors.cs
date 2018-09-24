47b1f9eb4d887d1fb5bab65147f8ef7a73e9b3c4
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-09-24 15:40:21 +0200

    fixed / my i had expired token

---
+++ filmowcy_projectUnity/Assets/Scripts/ShopButtonColors.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ShopButtonColors {
+
+	private static Color _defaultColor = new Color(0f, 0f, 0f, 0.39f);
+	private static Color _boughtColor = new Color(0f, 0.33f, 0f, 0.39f);
+	private static Color _disabledColor = new Color(0.5f, 0f, 0f, 0.39f);
+
+	public static Color Default {
+		get
+		{
+			return _defaultColor;
+		}
+
+		set { }
+	}
+
+	public static Color Bought
+	{
+		get
+		{
+			return _boughtColor;
+		}
+
+		set { }
+	}
+
+	public static Color Disabled
+	{
+		get
+		{
+			return _disabledColor;
+		}
+
+		set { }
+	}
+}
